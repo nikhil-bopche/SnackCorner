1fb033f7b94d41ee64a6cf179231da1e1c587424
Nikhil Bopche <ngbopche@gmail.com>
2024-08-23 17:16:11 +0530

    Added FoodItem Screen with details and tried to make Footer compoment responsive

--- src/Screens/FoodItemScreen.jsx
+++ src/Screens/FoodItemScreen.jsx
@@ -1,23 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import FoodItems from "../FoodItems.json";
 
 const FoodItemScreen = () => {
-//   const { id: FoodId } = useParams();
+  const { id: FoodId } = useParams();
+  const item = FoodItems.find((p) => p.id === FoodId);
+  console.log(item);
   return (
     <>
-      <div className="w-4/5 h-auto mx-auto flex">
+      <div className="w-4/5 h-auto mx-auto flex flex-col">
         <Link to="/">
           <div className="w-[100px] h-[50px] m-6 text-center flex justify-center items-center border-[1.5px] rounded shadow-lg shadow-slate-500">
             Go Back
           </div>
         </Link>
-        <div>
-          <div>
-            <img src="" alt="" />
+        <div className="w-full h-auto p-5 flex flex-wrap justify-around border-2 rounded-xl">
+          <div className="w-[45%]">
+            <img
+              src={item.imageSource}
+              alt={item.imageAltername}
+              className="m-auto rounded-lg"
+            />
           </div>
-          <div></div>
-          <div></div>
+          <div className="w-[30%] p-6">
+            <h1 className="font-bold font-mono text-3xl">{item.name}</h1>{" "}
+            <p className="text-slate-400 text-sm">({item.type})</p>
+            <hr />
+            <h2 className="mt-7">Description :- </h2>
+            <p className="text-sm">{item.description}</p>
+          </div>
+          <div className="w-[20%] p-6">
+            {" "}
+            <h1 className="font-bold font-mono text-lg">
+              Price :- {item.price}/-
+            </h1>
+            <hr />
+            <div>
+              <Link to="/">
+                <button className="mt-7 px-6 py-3 border-2 bg-blue-950 text-white rounded-md">
+                  Add
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <>
-      <div className="h-1/4 w-full mt-5 flex flex-wrap  bg-blue-950">
+      <div className="h-1/4 max-w-full mt-5 flex flex-wrap bg-blue-950">
         <div className="w-4/5 m-auto flex flex-wrap flex-row justify-start">
-          <div>
-            <div className="flex flex-wrap flex-row mr-[30px]">
-              <div className="w-8 my-[40px] mr-2">
-                <img src="./android-chrome-192x192.png" alt="" />
+          <div className="sm:ml-[50px] md:ml-[40px]  lg:w-1/4 md:w-2/4 sm:w-full">
+            <div className="flex flex-wrap flex-row">
+              <div className="w-8 my-[40px] sm:mr-2">
+                <img src="../android-chrome-192x192.png" alt="" />
               </div>
-              <div className="text-orange-500 my-[40px] font-bold text-2xl">
+              <div className="text-orange-500 my-[40px] font-bold text-2xl ">
                 SnackCorner
               </div>
             </div>
-            <div className="text-white my-[50px] mr-[30px] flex flex-wrap flex-row">
-              <img src="./White_location.png" alt="" className="w-5 h-5 mr-2" />
+            <div className="text-white my-[50px] mr-[30px] flex flex-wrap flex-row sm:my-[10px]">
+              <img src="../White_location.png" alt="LocationPNG" className="w-5 h-5 mr-2" />
               Nagpur, Maharastra
             </div>
           </div>
-          <div>
-            <ul className="text-white my-[50px]">
+          <div className="my-[50px] xl:ml-[100px] md:ml-[40px] sm:ml-[50px] lg:w-1/4 md:w-2/4 sm:w-full">
+            <h1 className="text-white font-bold mb-[50px]">About Us</h1>
+            <p className="text-slate-400 w-52 sm:w-44 text-[15px] text-justify">
+              Started in 1999. Since then serving love in the form of health and
+              taste. Everything here is lab tested,{" "}
+              <span className="font-bold">FSSAI</span> approved and we take
+              extra care of hygiene and cleanliness.
+            </p>
+          </div>
+          <div className="my-[50px] xl:ml-[100px] md:ml-[40px] sm:ml-[50px] lg:w-1/4 md:w-2/4 sm:w-full">
+            <ul>
               {" "}
-              Contact Us
-              <li> Instagram</li>
-              <li> Facebook</li>
-              <li> Instagram</li>
+              <h1 className="text-white font-bold mb-[50px]"> Contact Us</h1>
+              <li className="text-gray-400 flex items-center">
+                {" "}
+                <div className="inline-block">
+                  <img src="../instagram-48.png" alt="" className="w-5 m-1" />
+                </div>{" "}
+                <Link to="/" className="inline-block">Instagram</Link>
+              </li>
+              <li className="text-gray-400 flex items-center">
+                <div>
+                  <img src="../facebook-48.png" alt="" className="w-5 m-1" />
+                </div>
+                <Link to="/" className="inline-block"> Facebook </Link>
+              </li>
+              <li className="text-gray-400 flex items-center">
+                <div>
+                  <img src="../whatsapp-48.png" alt="" className="w-5 m-1" />
+                </div>
+                <Link to="/" className="inline-block"> What'sApp</Link>
+              </li>
             </ul>
           </div>
         </div>
